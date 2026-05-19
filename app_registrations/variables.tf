d7b9251c8e89f578b3778ba15105ee368b768572
milkicz <66957570+milkicz@users.noreply.github.com>
2026-05-19 16:26:34 +0200

    removed cluent id from variables.tf

--- app_registrations/variables.tf
+++ app_registrations/variables.tf
@@ -8,18 +8,6 @@ variable "tenant_id" {
   type        = string
 }
 
-variable "client_id" {
-  description = "Service principal client ID"
-  type        = string
-  sensitive   = true
-}
-
-variable "client_secret" {
-  description = "Service principal client secret"
-  type        = string
-  sensitive   = true
-}
-
 variable "environment" {
   description = "Environment name, for example dev, test, or prod."
   type        = string
